39485525c9ae4b42ed1ae4b0ca2461f0f4651f0d
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-07-21 00:07:57 +0200

    Add tests for Addition and substraction

--- CalcTests/PositionalSystem/NumberTests.cs
+++ CalcTests/PositionalSystem/NumberTests.cs
@@ -11,6 +11,216 @@ namespace Calc.PositionalSystem.Tests
     [TestClass()]
     public class NumberTests
     {
+        #region Math Operations Tests
+        // Math Operation Test Cases for operations with 2 operands
+        // 1. Two positive values
+        // 2. Two negative values
+        // 3. One Positive, One Negative
+        // 4. One Positive Value and Zero
+        // 5. One Negative Value and Zero
+        // 6. Two Zeroes
+        // 7. Overflow Check
+
+
+        #region Addition Tests
+        [TestMethod()]
+        public void Addition_TwoPositive_Pass()
+        {
+            var left = 10;
+            var right = 20.5;
+            var num1 = BaseConverter.ConvertToBase(left, 10);
+            var num2 = BaseConverter.ConvertToBase(right, 16);
+            var num3 = num1 + num2;
+
+            var expected = 30.5;
+
+            Assert.AreEqual(num3.DecimalValue, expected);
+        }
+
+        [TestMethod()]
+        public void Addition_TwoNegative_Pass()
+        {
+            var left = -10.0;
+            var right = -20.5;
+            var num1 = BaseConverter.ConvertToBase(left, 10);
+            var num2 = BaseConverter.ConvertToBase(right, 16);
+            var num3 = num1 + num2;
+
+            var expected = -30.5;
+
+            Assert.AreEqual(num3.DecimalValue, expected);
+        }
+
+        [TestMethod()]
+        public void Addition_PositiveNegatice_Pass()
+        {
+
+            var left = -10.0;
+            var right = 20.5;
+            var num1 = BaseConverter.ConvertToBase(left,10);
+            var num2 = BaseConverter.ConvertToBase(right,10);
+            var num3 = num1 + num2;
+
+            var expected = 10.5;
+
+            Assert.AreEqual(num3.DecimalValue, expected);
+        }
+
+        [TestMethod()]
+        public void Addition_PositiveZero_Pass()
+        {
+            var left = 10;
+            var right = 0.0;
+            var num1 = BaseConverter.ConvertToBase(left, 10);
+            var num2 = BaseConverter.ConvertToBase(right, 10);
+            var num3 = num1 + num2;
+
+            var expected = 10.0;
+
+            Assert.AreEqual(num3.DecimalValue, expected);
+        }
+
+        [TestMethod()]
+        public void Addition_NegativeZero_Pass()
+        {
+            var left = -10;
+            var right = 0;
+            var num1 = BaseConverter.ConvertToBase(left, 10);
+            var num2 = BaseConverter.ConvertToBase(right, 10);
+            var num3 = num1 + num2;
+
+            var expected = -10;
+
+            Assert.AreEqual(num3.DecimalValue, expected);
+        }
+
+        [TestMethod()]
+        public void Addition_TwoZeros_Pass()
+        {
+            var left = 0;
+            var right = 0;
+            var num1 = BaseConverter.ConvertToBase(left, 10);
+            var num2 = BaseConverter.ConvertToBase(right, 10);
+            var num3 = num1 + num2;
+
+            var expected = 0.0;
+
+            Assert.AreEqual(num3.DecimalValue, expected);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(OverflowException))]
+        public void Addition_Overflow_Fail()
+        {
+            var left = long.MaxValue;
+            var right = long.MaxValue;
+            var num1 = BaseConverter.ConvertToBase(left, 10);
+            var num2 = BaseConverter.ConvertToBase(right, 10);
+            var num3 = num1 + num2;
+        }
+
+        #endregion
+        #region Substraction Tests
+
+        [TestMethod()]
+        public void Substraction_TwoPositive_Pass()
+        {
+            var left = 20.5;
+            var right = 10.5;
+            var num1 = BaseConverter.ConvertToBase(left, 10);
+            var num2 = BaseConverter.ConvertToBase(right, 16);
+            var num3 = num1 - num2;
+
+            var expected = 10.0;
+
+            Assert.AreEqual(num3.DecimalValue, expected);
+        }
+
+        [TestMethod()]
+        public void Substraction_TwoNegative_Pass()
+        {
+            var left = -10.0;
+            var right = -20.5;
+            var num1 = BaseConverter.ConvertToBase(left, 10);
+            var num2 = BaseConverter.ConvertToBase(right, 16);
+            var num3 = num1 - num2;
+
+            var expected = 10.5;
+
+            Assert.AreEqual(num3.DecimalValue, expected);
+        }
+
+        [TestMethod()]
+        public void Substraction_PositiveNegatice_Pass()
+        {
+
+            var left = -10.0;
+            var right = 20.5;
+            var num1 = BaseConverter.ConvertToBase(left, 10);
+            var num2 = BaseConverter.ConvertToBase(right, 10);
+            var num3 = num1 - num2;
+
+            var expected = -30.5;
+
+            Assert.AreEqual(num3.DecimalValue, expected);
+        }
+
+        [TestMethod()]
+        public void Substraction_PositiveZero_Pass()
+        {
+            var left = 10;
+            var right = 0.0;
+            var num1 = BaseConverter.ConvertToBase(left, 10);
+            var num2 = BaseConverter.ConvertToBase(right, 10);
+            var num3 = num1 - num2;
+
+            var expected = 10.0;
+
+            Assert.AreEqual(num3.DecimalValue, expected);
+        }
+
+        [TestMethod()]
+        public void Substraction_NegativeZero_Pass()
+        {
+            var left = -10;
+            var right = 0;
+            var num1 = BaseConverter.ConvertToBase(left, 10);
+            var num2 = BaseConverter.ConvertToBase(right, 10);
+            var num3 = num1 - num2;
+
+            var expected = -10;
+
+            Assert.AreEqual(num3.DecimalValue, expected);
+        }
+
+        [TestMethod()]
+        public void Substraction_TwoZeros_Pass()
+        {
+            var left = 0;
+            var right = 0;
+            var num1 = BaseConverter.ConvertToBase(left, 10);
+            var num2 = BaseConverter.ConvertToBase(right, 10);
+            var num3 = num1 - num2;
+
+            var expected = 0.0;
+
+            Assert.AreEqual(num3.DecimalValue, expected);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(OverflowException))]
+        public void Substraction_Overflow_Fail()
+        {
+            var left = long.MinValue + 100;
+            var right = 10000;
+            var num1 = BaseConverter.ConvertToBase(left, 10);
+            var num2 = BaseConverter.ConvertToBase(right, 10);
+            var num3 = num1 - num2;
+        }
+
+        #endregion
+
+        #endregion
 
     }
 }
