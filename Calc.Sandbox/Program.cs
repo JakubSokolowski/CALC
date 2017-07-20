06b619955ae6befff4d349d19fba72cf3ad86422
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-07-21 00:09:20 +0200

    Add new Sandbox terminal app

---
+++ Calc.Sandbox/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calc.PositionalSystem;
+namespace Calc.Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = "-30.0";
+            var num2 = "-10.0";
+
+            var test1 = BaseConverter.ConvertToBase(num, 10 ,10);
+            var test2 = BaseConverter.ConvertToBase(num2, 10, 10);
+
+            var test3 = test1 / test2;
+
+            System.Console.WriteLine(test3.DecimalValue);
+            System.Console.ReadLine();
+         }
+    }
+}
