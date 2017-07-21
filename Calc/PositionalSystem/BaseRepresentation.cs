789da2621c959eadebdbd59b206ffa259eea0b57
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-07-21 22:46:10 +0200

    Add class to Represent characters avalible for given base. The BaseRepresentation replaces the static string with avalible characters (base 36 max) in prevoius version with <string,int> dictionary (capped to bas 256 - can be higher)

---
+++ Calc/PositionalSystem/BaseRepresentation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.PositionalSystem
+{
+    public class BaseRepresentation
+    {
+        public const int MAX_AVALIBLE_RADIX = 256;
+        private int currentRadix = 10;
+        readonly Dictionary<string, int> digitToIndexMap = new Dictionary<string, int>();
+        readonly Dictionary<string, int> digitToIndexUpToBase36Map = new Dictionary<string, int>();
+
+        public int CurrentRadix
+        {
+            get { return currentRadix; }
+            set
+            {
+                if (IsValidRadix(value))
+                    currentRadix = value;
+                else
+                    throw new ArgumentException("Radix must be between 2 and " + MAX_AVALIBLE_RADIX.ToString());
+            }
+        }
+
+        public BaseRepresentation(int maxRadix)
+        {
+            if (IsValidRadix(maxRadix))
+            {
+                String digitRepresentationString = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                for (int i = 0; i < 36; i++)
+                {
+                    digitToIndexUpToBase36Map.Add(digitRepresentationString.ElementAt(i).ToString(), i);
+                }               
+               
+                for (int i = 0; i < maxRadix; i++)
+                {
+                    string key = i.ToString();
+                    if (i < 10)
+                        key = "0" + key;
+                    digitToIndexMap.Add(key, i);
+                }               
+            }
+            else
+                throw new ArgumentException("Radix must be between 2 and " + MAX_AVALIBLE_RADIX.ToString());          
+        }
+
+        public BaseRepresentation() { }
+
+        public string GetDigit(int value)
+        {
+            if(currentRadix <= 36)
+            {
+                return digitToIndexUpToBase36Map.FirstOrDefault(x => x.Value == value).Key;
+            }
+            else
+            {
+                return digitToIndexMap.FirstOrDefault(x => x.Value == value).Key;
+            }           
+        }
+
+        public int GetValue(string key)
+        {
+            if(currentRadix <= 36)
+            {
+                return digitToIndexUpToBase36Map[key];
+            }
+            return digitToIndexMap[key];
+        }
+
+        private bool IsValidRadix(int radix) { return (radix >= 2 && radix <= 256); }
+    }
+}
