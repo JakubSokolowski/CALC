d7c90059ef791cd5e4a8f213c5c2213d53fb15b6
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-07-28 00:05:41 +0200

    Add enum for pages

---
+++ Calc.Desktop/DataModels/ApplicationPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.Desktop
+{
+    /// <summary>
+    /// A page of Application
+    /// </summary>
+    public enum ApplicationPage
+    {
+        /// <summary>
+        /// The Initial Welcome Page
+        /// </summary>
+        Welcome = 0,
+
+    }
+}
