c99f37e302b464787ce05b42b805b96a580b4c48
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-07-28 00:05:58 +0200

    Add enum for animations

---
+++ Calc.Desktop/Animation/PageAnimation.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Calc.Desktop.Animation
+{
+    /// <summary>
+    /// Styles of page animation
+    /// </summary>
+    public enum PageAnimation
+    {
+        None = 0,
+        SlideAndFadeFromRight = 1,
+        SlideAndFadeOutToLeft = 2,
+        FadeIn = 3,
+        FadeOut = 4,
+    }
+}
