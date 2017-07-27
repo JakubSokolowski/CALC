9b9b25a1b4ac9f988f2e96d0e810c043fbe09160
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-07-28 00:06:54 +0200

    Ad value Converter folder. Add base value converter

---
+++ Calc.Desktop/ValueConverters/ApplcationPageValueConverter.cs
@@ -0,0 +1,31 @@
+﻿ 
+using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Calc.Desktop
+{   
+    /// <summary>
+    /// Convertes the <see cref="ApplicationPage"/> to actual view/page
+    /// </summary>
+    public class ApplicationPageValueConverter : ValueConverter<ApplicationPageValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Find the appropriate page
+            switch((ApplicationPage)value)
+            {
+                case ApplicationPage.Welcome:
+                    return new WelcomePage();
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Calc.Desktop/ValueConverters/ValueConverter.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Calc.Desktop
+{
+    // The same as static resource, bindings 
+    // TODO read more
+
+    /// <summary>
+    /// Base Value Converter that allows direct XAML usage 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class ValueConverter<T> : MarkupExtension, IValueConverter
+        where T : class, new()
+    {
+
+        private static T mConverter = null;
+
+        #region Value Converter Methods
+
+
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+        public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return mConverter ?? (mConverter = new T());
+        }
+
+        #endregion
+
+    }
+}
