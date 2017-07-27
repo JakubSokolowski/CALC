c7af1222cc479895ec5a32b05bd9b6780d4b81ba
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-07-27 23:54:56 +0200

    Add to Base Conversion folder

---
+++ Calc/PositionalSystem/BaseConversion/IBaseConversion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.PositionalSystem
+{
+    public interface IBaseConversion
+    {
+        String DecimalIntegerToArbitraryBase(long decimalNumber, int radix);
+        String DecimalFractionToArbitraryBase(double fraction, int radix);
+
+        long ArbitraryBaseToDecimal(String valueString, int radix);
+        double ArbitraryFractionToDecimal(String valueString, int radix);
+    }
+}
