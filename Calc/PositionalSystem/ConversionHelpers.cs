d5c71b2241af9beaa07131694fafa54548550dca
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-07-27 23:58:33 +0200

    Extract cross-class helper methods to helper class

---
+++ Calc/PositionalSystem/ConversionHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.PositionalSystem
+{
+    public static class ConversionHelpers
+    {
+        public static List<string> RepresentationStringToStringList(string str, int radix)
+        {
+            var strList = new List<String>();
+            if (radix <= 36)
+            {
+                strList = str.ToCharArray().Select(x => x.ToString()).ToList();
+            }
+            else
+            {
+                strList = str.Split(' ').ToList();
+            }
+            return strList;
+        }
+    }
+}
