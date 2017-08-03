6493f518056236702923bf3c146cfbe085143bb0
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-08-03 15:29:53 +0200

    Make the WelcomePage inherit from BasePage

--- Calc.Desktop/Pages/WelcomePage.xaml.cs
+++ Calc.Desktop/Pages/WelcomePage.xaml.cs
@@ -1,28 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using Calc.Desktop;
 
 namespace Calc.Desktop
 {
     /// <summary>
     /// Interaction logic for WelcomePage.xaml
     /// </summary>
-    public partial class WelcomePage : Page
+    public partial class WelcomePage : BasePage
     {
         public WelcomePage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+           this.AnimateOut();
         }
     }
 }
