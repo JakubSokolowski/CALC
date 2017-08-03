077bdeff9fe7fb2cb2888e24a7326ca02ceacfb5
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-08-03 15:26:47 +0200

    Add enum for different types of Page Animation

---
+++ Calc.Desktop/Animation/AnimationStyles.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Calc.Desktop.Animation
+{
+    /// <summary>
+    /// Styles of page animation
+    /// </summary>
+    public enum AnimationStyles
+    {
+        None ,
+        SlideAndFadeFromBottom,
+        SlideAndFadeOutToBottom,
+        SlideAndFadeOutToLeft,
+        FadeIn,
+        FadeOut,
+    }
+}
