3b70369c3229c3657beee1dbe9f78e2c2c05c3ae
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-08-04 21:16:53 +0200

    Add ViewModel for Welcome Page

---
+++ Calc.Desktop/ViewModel/WelcomeViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Threading.Tasks;
+
+namespace Calc.Desktop
+{
+    public class WelcomeViewModel : BaseViewModel
+    {
+        #region Private Members        
+       
+
+        #endregion
+
+        #region Public Properties
+
+        public ICommand StartCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Defualt Constructor
+        /// </summary>
+        /// <param name="window"></param>
+        public WelcomeViewModel(Window window)
+        {
+                       // Create Commands           
+            StartCommand = new RelayCommand(async () =>  await StartMainPage() );
+
+            // Fix window resize issue
+          
+
+        }
+
+        public async Task StartMainPage()
+        {
+            await Task.Delay(500);
+        }
+        #endregion
+
+        #region Private Helpers
+
+       
+        #endregion
+
+    }
+}
