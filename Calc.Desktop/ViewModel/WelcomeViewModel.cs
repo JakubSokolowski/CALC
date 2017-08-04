065b1abf1d3b0b42ff7c65e895909a16063601d9
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-08-04 23:35:02 +0200

    Change constructor to without parameters

--- Calc.Desktop/ViewModel/WelcomeViewModel.cs
+++ Calc.Desktop/ViewModel/WelcomeViewModel.cs
@@ -5,31 +5,20 @@ using System.Threading.Tasks;
 namespace Calc.Desktop
 {
     public class WelcomeViewModel : BaseViewModel
-    {
-        #region Private Members        
-       
-
-        #endregion
+    {     
 
         #region Public Properties
 
         public ICommand StartCommand { get; set; }
 
+
         #endregion
 
         #region Constructor
-        /// <summary>
-        /// Defualt Constructor
-        /// </summary>
-        /// <param name="window"></param>
-        public WelcomeViewModel(Window window)
-        {
-                       // Create Commands           
-            StartCommand = new RelayCommand(async () =>  await StartMainPage() );
-
-            // Fix window resize issue
-          
-
+     
+        public WelcomeViewModel()
+        {                     
+            StartCommand = new RelayCommand(async () =>  await StartMainPage());
         }
 
         public async Task StartMainPage()
