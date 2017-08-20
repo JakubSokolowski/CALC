dc5eeced8f85f3b07b5d1b3162d39180cd00cd52
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-08-20 20:44:02 +0200

    Add models for user controls

---
+++ Calc.Desktop/ViewModels/Controls/ButtonListViewModel.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.Desktop
+{
+    public class ButtonListViewModel : BaseViewModel
+    {
+        public BindingList<SquareButtonViewModel> Buttons { get; set; } 
+        
+
+        public ButtonListViewModel()
+        {
+           Buttons = new BindingList<SquareButtonViewModel>();
+        }
+
+        public void AddButtons(int buttonCount)
+        {
+            for (int i = 0; i < buttonCount; i++)
+            {
+                Buttons.Add(new SquareButtonViewModel());                
+            }
+        }
+
+        public void AddButtons(int buttonCount, string content)
+        {
+            for (int i = 0; i < buttonCount; i++)
+            {
+                Buttons.Add(new SquareButtonViewModel { SingleCharContent = content, Tag = "DUPA" });
+            }
+        }
+
+        public void WriteText(string text)
+        {
+            Buttons.Clear();
+            for(int i = 0; i < text.Length;i++)
+            {
+                var button = new SquareButtonViewModel();
+                button.SingleCharContent = text[i].ToString();
+                button.Tag = "AYY LMOA";
+                Buttons.Add(button);
+              
+            }
+        }
+        public string GetContent()
+        {
+            string content = "";
+            foreach(var button in Buttons)
+            {
+                content += button.SingleCharContent;
+            }
+            return content;
+        }
+    }
+}
---
+++ Calc.Desktop/ViewModels/Controls/SquareButtonViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calc.Desktop
+{
+    public class SquareButtonViewModel : BaseViewModel
+    {
+        private string mContent= "0";
+
+        public string Tag { get; set; } = "DEFAULT CONSTRUCTED";
+
+        public ICommand SquareButtonCommand { get; set; }
+        public string SingleCharContent { get => mContent; set => mContent = value; }
+        public LinePosition Position { get; set; }
+
+        public SquareButtonViewModel( )
+        {
+            SquareButtonCommand = new RelayCommand(() => FlipBit());
+        }
+
+        public void FlipBit()
+        {
+            SingleCharContent = (SingleCharContent == "0") ? "1" : "0";
+          
+        }
+    }
+}
