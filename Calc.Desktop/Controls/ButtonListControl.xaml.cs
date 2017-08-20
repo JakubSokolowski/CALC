87b638aee6750d422be98f2e0a1e33f90b57d733
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-08-20 17:21:56 +0200

    Add Controls for SquareButton and Button List

---
+++ Calc.Desktop/Controls/ButtonListControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+
+namespace Calc.Desktop
+{
+    /// <summary>
+    /// Interaction logic for ButtonListControl.xaml
+    /// </summary>
+    public partial class ButtonListControl : UserControl
+    {
+        public ButtonListControl()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
