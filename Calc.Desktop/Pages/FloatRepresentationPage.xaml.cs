b62669275703d57062f0e2f83173a1dc5eb32d37
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-08-20 20:43:12 +0200

    Add page for IEEE 754 FloatRepresentation and conversion

---
+++ Calc.Desktop/Pages/FloatRepresentationPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+using Calc.PositionalSystem;
+
+namespace Calc.Desktop
+{
+
+    public partial class FloatRepresentationPage : BasePage<FloatRepresentationViewModel>
+    {
+        public FloatRepresentationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
