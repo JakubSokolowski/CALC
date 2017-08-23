bae288280873b8a106a7c2e0c26227cc4f1b567b
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-08-23 23:53:25 +0200

    Implement Mediator pattern to help with communications between viewmodels

---
+++ Calc.Desktop/ViewModels/Mediator/Mediator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.Desktop
+{
+    public sealed class Mediator
+    {
+        static readonly Mediator mInstance = new Mediator();
+        private volatile object mLocker = new object();
+
+        MultiDictionary<ViewModelMessages, Action<Object>> mInternalList = new MultiDictionary<ViewModelMessages, Action<object>>();
+
+        static Mediator() { }
+        private Mediator() { }
+
+        public static Mediator Instance => mInstance;
+
+        public void Register(Action<object> callback, ViewModelMessages message)
+        {
+            mInternalList.AddValue(message, callback);
+        }
+
+        public void NotifyColleagues(ViewModelMessages message, object args)
+        {
+            if(mInternalList.ContainsKey(message))
+            {
+                // forward the message to all listeners
+                foreach (var callback in mInternalList[message])
+                    callback(args);              
+            }
+        }
+    }
+}
