97602023f30a338c67a1622e602a2adad0be9e3a
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-08-28 13:18:55 +0200

    Add foundation for inversion of control pattern

---
+++ Calc.Desktop/IoC/IoC.cs
@@ -0,0 +1,28 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.Desktop
+{
+    public static class IoC
+    {
+        public static IKernel Kernel { get; private set; } = new StandardKernel();
+
+
+        // Binds all the information required
+        public static void Setup()
+        {
+            BindViewModels();
+        }
+
+        // Bind all Singleton viewmodels
+        private static void BindViewModels()
+        {
+        
+        }
+    }
+
+}
