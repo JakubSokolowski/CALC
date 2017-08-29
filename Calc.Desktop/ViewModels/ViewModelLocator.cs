1bbb3a16365170c3b4c6d4cd5304a9c7c3b86e96
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-08-29 20:56:11 +0200

    Add class that allows locating viewmodels bound in IoC kernel

---
+++ Calc.Desktop/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,11 @@
+﻿using Calc.Core;
+
+
+namespace Calc.Desktop
+{
+    public class ViewModelLocator
+    {
+        public static ViewModelLocator Instance { get; private set; } = new ViewModelLocator();
+        public static ApplicationViewModel ApplicationViewModel => IoC.Get<ApplicationViewModel>();
+    }
+}
