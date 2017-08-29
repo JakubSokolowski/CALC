911e61b07e8846bf6bf2e110ed2bd0f0c5e32eff
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-08-29 20:55:33 +0200

    Change the startup method in application, to allow setting up the Inversion of Control pattern

--- Calc.Desktop/App.xaml.cs
+++ Calc.Desktop/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
+using Calc.Core;
 using System.Windows;
 
 namespace Calc.Desktop
@@ -13,5 +9,14 @@ namespace Calc.Desktop
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            IoC.Setup();
+
+            // Show the main window
+            Current.MainWindow = new MainWindow();
+            Current.MainWindow.Show();
+        }
     }
 }
