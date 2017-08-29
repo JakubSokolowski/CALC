da9e62dcb733abc74823de82d89d295c9d86bd22
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-08-29 20:54:37 +0200

    Converter that returns pages from Kernel binding

---
+++ Calc.Desktop/ValueConverters/IoCConverter.cs
@@ -0,0 +1,30 @@
+﻿
+using Calc.Core;
+using Ninject;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Calc.Desktop
+{      
+    public class IoCConverter : ValueConverter<IoCConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Find the appropriate page
+            switch((string)parameter)
+            {
+                case nameof(ApplicationViewModel):
+                    return IoC.Get<ApplicationViewModel>();
+                default:               
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
