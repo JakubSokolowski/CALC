f775d9ada2f88c513cff6f6e52e62653551383e3
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-08-29 20:56:40 +0200

    Fix the current page binding.

--- Calc.Desktop/MainWindow.xaml.cs
+++ Calc.Desktop/MainWindow.xaml.cs
@@ -1,12 +1,10 @@
-﻿using System.Windows;
+﻿using Calc.Core;
+using System.Windows;
 
 namespace Calc.Desktop
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+{ 
     public partial class MainWindow : Window
-    {
+    {       
         public MainWindow()
         {
             InitializeComponent();
