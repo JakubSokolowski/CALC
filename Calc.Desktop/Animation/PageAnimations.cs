89f6964c68a53bfb90ec773f4dbb6cfcd9694277
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-08-31 00:51:28 +0200

    Add more directions of animations

--- Calc.Desktop/Animation/PageAnimations.cs
+++ Calc.Desktop/Animation/PageAnimations.cs
@@ -1,5 +1,4 @@
 ﻿
-
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,30 +11,20 @@ namespace Calc.Desktop
         public static async Task SlideAndFadeInFromTheBottom(this Page page, float seconds)
         {
             var sb = new Storyboard();
-
             sb.AddSlideFromBottom(seconds, page.WindowHeight);
-
             sb.AddFadeIn(seconds);
-
             sb.Begin(page);
-
             page.Visibility = Visibility.Visible;
-
             await Task.Delay((int)(seconds * 1000));
         }
 
         public static async Task SlideAndFadeOutToBottom(this Page page, float seconds)
         {
             var sb = new Storyboard();
-
             sb.AddSlideToBottom(seconds, page.WindowHeight);
-
             sb.AddFadeOut(seconds);
-
             sb.Begin(page);
-
             page.Visibility = Visibility.Visible;
-
             await Task.Delay((int)(seconds * 1000));
         }
 
--- Calc.Desktop/Animation/StoryboardHelpers.cs
+++ Calc.Desktop/Animation/StoryboardHelpers.cs
@@ -7,7 +7,7 @@ namespace Calc.Desktop
 {
     public static class StoryboardHelpers
     {
-        public static void AddSlideFromBottom(this Storyboard storyboard, float seconds, double offset, float decelerationRatio = 0.9f)
+        public static void AddSlideFromBottom(this Storyboard storyboard, float seconds, double offset, float decelerationRatio = 0.9f, bool keepMargin = true)
         {
             // Create the margin animate from bottom
             var slideAnimation = new ThicknessAnimation
@@ -21,8 +21,7 @@ namespace Calc.Desktop
             Storyboard.SetTargetProperty(slideAnimation, new PropertyPath("Margin"));
             storyboard.Children.Add(slideAnimation);
         }
-
-        public static void AddSlideToBottom(this Storyboard storyboard, float seconds, double offset, float decelerationRatio = 0.9f)
+        public static void AddSlideToBottom(this Storyboard storyboard, float seconds, double offset, float decelerationRatio = 0.9f, bool keepMargin = true)
         {
             // Create the margin animate from bottom
             var slideAnimation = new ThicknessAnimation
@@ -35,6 +34,76 @@ namespace Calc.Desktop
 
             Storyboard.SetTargetProperty(slideAnimation, new PropertyPath("Margin"));
             storyboard.Children.Add(slideAnimation);
+        }
+
+        public static void AddSlideFromLeft(this Storyboard storyboard, float seconds, double offset, float decelerationRatio = 0.9f, bool keepMargin = true)
+        {
+            // Create the margin animate from right 
+            var animation = new ThicknessAnimation
+            {
+                Duration = new Duration(TimeSpan.FromSeconds(seconds)),
+                From = new Thickness(-offset, 0, keepMargin ? offset : 0, 0),
+                To = new Thickness(0),
+                DecelerationRatio = decelerationRatio
+            };
+
+            // Set the target property name
+            Storyboard.SetTargetProperty(animation, new PropertyPath("Margin"));
+
+            // Add this to the storyboard
+            storyboard.Children.Add(animation);
+        }       
+        public static void AddSlideToLeft(this Storyboard storyboard, float seconds, double offset, float decelerationRatio = 0.9f, bool keepMargin = true)
+        {
+            // Create the margin animate from right 
+            var animation = new ThicknessAnimation
+            {
+                Duration = new Duration(TimeSpan.FromSeconds(seconds)),
+                From = new Thickness(0),
+                To = new Thickness(-offset, 0, keepMargin ? offset : 0, 0),
+                DecelerationRatio = decelerationRatio
+            };
+
+            // Set the target property name
+            Storyboard.SetTargetProperty(animation, new PropertyPath("Margin"));
+
+            // Add this to the storyboard
+            storyboard.Children.Add(animation);
+        }
+
+        public static void AddSlideFromRight(this Storyboard storyboard, float seconds, double offset, float decelerationRatio = 0.9f, bool keepMargin = true)
+        {
+            // Create the margin animate from right 
+            var animation = new ThicknessAnimation
+            {
+                Duration = new Duration(TimeSpan.FromSeconds(seconds)),
+                From = new Thickness(keepMargin ? offset : 0, 0, -offset, 0),
+                To = new Thickness(0),
+                DecelerationRatio = decelerationRatio
+            };
+
+            // Set the target property name
+            Storyboard.SetTargetProperty(animation, new PropertyPath("Margin"));
+
+            // Add this to the storyboard
+            storyboard.Children.Add(animation);
+        }       
+        public static void AddSlideToRight(this Storyboard storyboard, float seconds, double offset, float decelerationRatio = 0.9f, bool keepMargin = true)
+        {
+            // Create the margin animate from right 
+            var animation = new ThicknessAnimation
+            {
+                Duration = new Duration(TimeSpan.FromSeconds(seconds)),
+                From = new Thickness(0),
+                To = new Thickness(keepMargin ? offset : 0, 0, -offset, 0),
+                DecelerationRatio = decelerationRatio
+            };
+
+            // Set the target property name
+            Storyboard.SetTargetProperty(animation, new PropertyPath("Margin"));
+
+            // Add this to the storyboard
+            storyboard.Children.Add(animation);
         }
 
         public static void AddFadeIn(this Storyboard storyboard, float seconds)
@@ -50,7 +119,6 @@ namespace Calc.Desktop
             Storyboard.SetTargetProperty(slideAnimation, new PropertyPath("Opacity"));
             storyboard.Children.Add(slideAnimation);
         }
-
         public static void AddFadeOut(this Storyboard storyboard, float seconds)
         {
             // Create the margin animate from bottom
