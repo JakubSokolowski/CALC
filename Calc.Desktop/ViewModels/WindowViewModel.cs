daa6681ad1432de9e7198572bea5622cfe893b4c
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-09-04 00:12:46 +0200

    Add view model for side menu

--- Calc.Desktop/ViewModels/WindowViewModel.cs
+++ Calc.Desktop/ViewModels/WindowViewModel.cs
@@ -6,45 +6,47 @@ namespace Calc.Desktop
 {
     public class WindowViewModel : BaseViewModel
     {
-       
+
         private Window mWindow;
-        private int mOuterMarginSize = 10;       
+        private int mOuterMarginSize = 10;
         private int mWindowRadius = 10;
         private WindowDockPosition mDockPosition = WindowDockPosition.Undocked;
 
 
         public double WindowMinimumWidth { get; set; } = 550;
-        public double WindowMinimumHeight { get; set; } = 400;  
-        
-        public int ResizeBorder { get; set; } = 6;  
-        public Thickness ResizeBorderThickness => new Thickness(ResizeBorder + OuterMarginSize); 
+        public double WindowMinimumHeight { get; set; } = 400;
 
-        public Thickness InnerContentPadding => new Thickness(ResizeBorder); 
+        public int ResizeBorder { get; set; } = 6;
+        public Thickness ResizeBorderThickness => new Thickness(ResizeBorder + OuterMarginSize);
+
+        public Thickness InnerContentPadding => new Thickness(ResizeBorder);
 
         public int OuterMarginSize
         {
             get => mWindow.WindowState == WindowState.Maximized ? 0 : mOuterMarginSize;
             set => mOuterMarginSize = value;
-        }       
-        public Thickness OuterMarginSizeThickness => new Thickness(OuterMarginSize); 
+        }
+        public Thickness OuterMarginSizeThickness => new Thickness(OuterMarginSize);
 
         public int WindowRadius
         {
-            get => mWindow.WindowState == WindowState.Maximized ? 0 : mWindowRadius;           
+            get => mWindow.WindowState == WindowState.Maximized ? 0 : mWindowRadius;
             set => mWindowRadius = value;
         }
-        public CornerRadius WindowCornerRadius => new CornerRadius(OuterMarginSize);  
-        
-        public int TitleHeight { get; set; } = 42;
-        public GridLength TitleHeightGridLenght => new GridLength(TitleHeight + ResizeBorder);       
-        
+        public CornerRadius WindowCornerRadius => new CornerRadius(OuterMarginSize);
 
-      
+        public int TitleHeight { get; set; } = 42;
+        public GridLength TitleHeightGridLenght => new GridLength(TitleHeight + ResizeBorder);
+
+
+
         public ICommand MinimizeCommand { get; set; }
         public ICommand MaximizeCommand { get; set; }
         public ICommand CloseCommand { get; set; }
         public ICommand MenuCommand { get; set; }
-     
+
+        public SideMenuViewModel SideMenu { get; set; } = new SideMenuViewModel();
+
         public WindowViewModel(Window window) 
         {
             mWindow = window;
