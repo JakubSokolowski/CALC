0029ec0e00a6fbb8b6319f813650f69307f9a64d
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-09-04 00:08:49 +0200

    Add view model class for SideMenu

---
+++ Calc.Core/ViewModels/Controls/SideMenuViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calc.Core
+{
+    public class SideMenuViewModel : BaseViewModel
+    {
+
+        public ICommand GoToBaseConverterCommand { get; set; }
+        public ICommand GoToFloatConverterCommand { get; set; }
+
+
+        public SideMenuViewModel()
+        {
+            GoToBaseConverterCommand = new RelayCommand(() => GoToBaseConverter());
+            GoToFloatConverterCommand = new RelayCommand(() => GoToFloatConverter());
+        }
+
+        public void GoToBaseConverter()
+        {
+            IoC.Get<ApplicationViewModel>().SideMenuVisible = true;
+            IoC.Get<ApplicationViewModel>().CurrentPage = ApplicationPage.BaseConverter;
+        }
+        public void GoToCalculator()
+        {
+
+        }
+        public void GoToFloatConverter()
+        {
+            IoC.Get<ApplicationViewModel>().SideMenuVisible = true;
+            IoC.Get<ApplicationViewModel>().CurrentPage = ApplicationPage.FloatRepresentation;
+        }
+    }
+}
